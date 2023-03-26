c8bde51a030f517d2c3893448aa1ed20ddc8817f
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-03-25 22:31:50 -0400

    fix: changed type of matches

--- models/team.py
+++ models/team.py
@@ -2,15 +2,18 @@
 
 from dataclasses import dataclass
 
+from match import Match
+
 @dataclass
 class Team:
-    """A football team in the premier league.
+    """A football team in the Premier League in a particular season.
 
     Instance Attributes:
         - name: the name of this team
+        - matches: a chronological list of the matches played by this team in the season
 
     Representation Invariants:
         ...
     """
     name: str
-    
+    matches: list[Match]
