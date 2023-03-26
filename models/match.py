ddd2501a8cbeff47099f9f9619ef84bbb64a294e
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-03-26 14:25:36 -0400

    feat: separated teams into home and away

--- models/match.py
+++ models/match.py
@@ -6,24 +6,30 @@ from enum import Enum
 from typing import Optional
 from dataclasses import dataclass
 
+import team
+
 
 @dataclass
 class Match:
     """A Premier League match between two teams in a particular season.
 
     Instance Attributes:
-        - teams: A set of the the two teams names that played this match.
+        - home_team: The team playing the match at home. 
+        - away_team: The team playing the match away from their home ground. 
         - date_time: The date and time of the match played.
-        - winner: The name of the winning team of the match, or None if the match ended in a draw.
         - details: A mapping from each team name to its corresponding match details.
+        - result: the result of this match.
 
     Representation Invariants:
-        - len(teams) == 2
+        - len(details) == 2
+        - home_team.name in details
+        - away_team.name in details
     """
-    teams: set[str]
+    home_team: team.Team
+    away_team: team.Team
     date_time: datetime
     details: dict[str, MatchDetails]
-    winner: Optional[str]
+    result: MatchResult
 
 
 @dataclass
@@ -31,20 +37,26 @@ class MatchDetails:
     """The details of a team's performance in a Premier League match.
 
     Instance Attributes:
-        - fouls_commited: number of fouls commited by the team in the match
-        - red_cards: number of red cards given to the team in the match
-        - yellow_cards: number of yellow cards given to the team in the match
-        - offsides: number of offsides awarded to the team in the match 
-        - half_time_goals: number of goals scored by the team at half time
-        - full_time_goals: number of goals scored by the team at full time
+        - match: The match that this match detail refers to.
+        - team: The team that these details are about.
+        - fouls_commited: number of fouls commited by the team in the match.
+        - red_cards: number of red cards given to the team in the match.
+        - yellow_cards: number of yellow cards given to the team in the match.
+        - offsides: number of offsides awarded to the team in the match.
+        - half_time_goals: number of goals scored by the team at half time.
+        - full_time_goals: number of goals scored by the team at full time.
 
     Representation Invariants:
         ...
     """
+    match: Match
+    team: team.Team
     fouls_commited: int
     red_cards: int
     yellow_cards: int
     offsides: int
     half_time_goals: int
     full_time_goals: int
-    
+
+
+MatchResult = Enum('MatchResult', ['HOME_WIN', 'AWAY_WIN', 'DRAW'])
