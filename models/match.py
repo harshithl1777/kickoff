6f6280efeba7e6d0c40076c9cf35f2ce1c24dbc6
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-03-25 21:11:40 -0400

    fix: removed timed goals from match class

--- models/match.py
+++ models/match.py
@@ -18,15 +18,12 @@ class Match:
         - date_time: the date and time of the match played
         - winner: the winner of the match, or None if the match ended in a draw
         - details: a dictionary mapping each teams to its corresponding match details
-        - goals: a list of tuples, where each tuple represents a goal and is of the form:
-          (minute the goal was scored, team that scored the goal)   
 
     Representation Invariants:
         - len(teams) == 2
     """
     teams: set[Team]
     date_time: datetime
-    goals: list[tuple[int, Team]]
     details: dict[Team, MatchDetails]
     winner: Optional[Team]
 
@@ -37,10 +34,12 @@ class MatchDetails:
     """The details of a team's performance in a Premier League match.
 
     Instance Attributes:
-        - fouls_commited
-        - red_cards
-        - yellow_cards
-        - offsides
+        - fouls_commited: number of fouls commited by the team in the match
+        - red_cards: number of red cards given to the team in the match
+        - yellow_cards: number of yellow cards given to the team in the match
+        - offsides: number of offsides awarded to the team in the match 
+        - half_time_goals: number of goals scored by the team at half time
+        - _timfulle_goals: number of goals scored by the team at full time
 
     Representation Invariants:
         ...
@@ -49,3 +48,6 @@ class MatchDetails:
     red_cards: int
     yellow_cards: int
     offsides: int
+    half_time_goals: int
+    full_time_goals: int
+    
