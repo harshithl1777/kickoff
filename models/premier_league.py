d909ca38709b1ac7832c26865a7e1e09609f2661
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-03-27 17:50:10 -0400

    feat: added seasons enum in premier_league

--- models/premier_league.py
+++ models/premier_league.py
@@ -1,5 +1,6 @@
 """Module containing the Season class and other related components"""
 
+from enum import Enum
 
 from team import Team
 from match import Match
@@ -7,7 +8,6 @@ from match import Match
 
 class PremierLeague:
     """A graph-based representation of Premier League matches and teams.
-
 
     Instance Attributes:
         - teams: 
@@ -17,9 +17,26 @@ class PremierLeague:
         - matches:
             A chronologically ordered list of all matches played in this season.
 
-
     Representation Invariants:
         ...
     """
     teams: dict[str, Team]
     matches: list[Match]
+
+
+"""
+All Premier League seasons in the dataset.
+Season.ABCD corresponds to the 20AB - 20CD season.
+"""
+Seasons = Enum("Seasons",
+               ["0910",
+                "1011",
+                "1112",
+                "1213",
+                "1314"
+                "1415",
+                "1516",
+                "1617",
+                "1718",
+                "1819"]
+               )
