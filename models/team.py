b590315307def5fcb8241f038c29e9790c31f620
Harshith <harshithl1777@gmail.com>
2023-03-27 18:44:17 -0400

    fix: change to single graph representation

--- models/team.py
+++ models/team.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
+from python_ta.contracts import check_contracts
+from typing import Literal
 
 
 from match import Match
-import premier_league
 
+
+@check_contracts
 @dataclass
 class Team:
     """A football team playing in a particular season of the Premier League.
@@ -17,9 +20,14 @@ class Team:
         - seasons: The seasons this team has participated in.
 
     Representation Invariants:
-        ...
+        - len(self.matches) > 0
     """
+
     name: str
     matches: list[Match]
-    seasons: list[premier_league.Seasons]
-    
+
+
+if __name__ == "__main__":
+    import python_ta
+
+    python_ta.check_all()
