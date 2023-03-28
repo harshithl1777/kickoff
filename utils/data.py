7e62935231b4e8a3008478332e21caae727e660d
muhammadIbrahim-cpu <66919696+muhammadIbrahim-cpu@users.noreply.github.com>
2023-03-28 12:48:09 -0400

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -10,6 +10,9 @@ import pandas as pd
 from python_ta.contracts import check_contracts
 
 from utils.constants import Constants
+from models.league import League
+from models.match import Match, MatchDetails
+from models.team import Team
 
 
 @check_contracts
@@ -27,10 +30,34 @@ def generate_pandas_df(csv_file: str) -> pd.DataFrame:
 
 
 @check_contracts
-def convert_to_graph(dataframe: pd.DataFrame) -> ...:
-    """Muhammad, please write this docstring"""
-    raise NotImplementedError
+def convert_to_graph(dataframe: pd.DataFrame, league: League) -> None:
+    """Populate the graph with the provided dataframe representing the match and overall season statistics
 
+    Preconditions:
+        - dataframe is a valid representation of a csv file stored in the assets folder
+    """
+
+    # TODO: Not sure how to work with pandas dataframe, someone take a look and edit as necessary
+    for row in dataframe:  
+        if row[home_team] not in league.teams:
+            home_team = Team(row["HomeTeam"], [])
+        else:
+            home_team = league.teams[row["HomeTeam"]]
+
+        if row[away_team] not in league.teams:
+            away_team = Team(row["AwayTeam"], [])
+        else:
+            away_team = league.teams[row["AwayTeam"]]
+        
+
+        home_team_details = MatchDetails(home_team, row["HF"], row["HS"], row["HST"], row["HR"], row["HY"], row["HTHG"], row["FTHG"])
+        away_team_details = MatchDetails(away_team, row["AF"], row["AS"], row["AST"], row["AR"], row["AY"], row["HTAG"], row["FTAG"])
+        result_status = {"H": "HOME_WIN", "A": "AWAY_WIN", "D": "DRAW"}
+
+        # TODO: Fix date and season attribute
+        match = Match('season', home_team, away_team, 'date', {home_team: home_team_details, away_team: away_team_details}, result_status[row["FTR"]])
+        home_team.matches.append(match)
+        away_team.mathes.append(match)
 
 if __name__ == "__main__":
     import python_ta
